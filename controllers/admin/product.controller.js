15a524a219219cb930358c8f7109f913d7d11a02
Nguyễn Anh Tuấn <tuannguyen10112004@gmail.com>
2024-10-29 19:58:23 +0700

    Tính năng tìm kiếm (front end)

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,9 +3,11 @@ const Car_items = require("../../models/product.model");
 //[GET] /admin/car_items
 module.exports.index = async (req, res) => {
 
-    const cars = await Car_items.find({
-        // deleted = false;
-    });
+    let find = {
+        deleted: false
+    };
+
+    const cars = await Car_items.find(find);
 
     console.log(cars)
     res.render("admin/pages/products/index", {

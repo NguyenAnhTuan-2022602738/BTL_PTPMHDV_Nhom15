e23c305d09ff693184bafb15034e977a6c2955b2
Nguyễn Anh Tuấn <tuannguyen10112004@gmail.com>
2024-10-29 10:14:49 +0700

    install and require mongoose

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const route = require("./routes/client/index.route");
+
+mongoose.connect("mongodb://127.0.0.1:27017/BTL_PTPMHDV");
+
 
 const app = express();
 const port = process.env.PORT;

716c989019f6c14a9ca587ec213b989082944b18
Nguyễn Anh Tuấn <tuannguyen10112004@gmail.com>
2024-10-31 23:34:30 +0700

    Cập nhật tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,6 @@
 const Car_items = require("../../models/product.model");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 //[GET] /admin/car_items
 module.exports.index = async (req, res) => {
 
@@ -17,22 +18,18 @@ module.exports.index = async (req, res) => {
 
 
     //pagination
-    let objectPagination = {
-        currentPage: 1,
-        limitItems: 10
-    }
-
-    if(req.query.page){
-        objectPagination.currentPage = parseInt(req.query.page);
-    }
-
-    objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
     const countCars = await Car_items.countDocuments(find);
-    const totalPage = Math.ceil(countCars / objectPagination.limitItems);
-    objectPagination.totalPage = totalPage;
-    //End pagination
 
+    let objectPagination = paginationHelper(
+        {
+            currentPage: 1,
+            limitItems: 10,
+        },
+        req.query,
+        countCars
+    );
+
+  
     const cars = await Car_items.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
     
     res.render("admin/pages/products/index", {
